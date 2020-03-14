420a851c5fa5557cc75d052f22eacc9574c35528
Carlos Ramirez <calbertora@gmail.com>
2020-03-13 20:56:34 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterfacesCsharp8
+{
+
+    public interface ICustomer
+    {
+        IEnumerable<IOrder> PreviousOrder{get;}
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
